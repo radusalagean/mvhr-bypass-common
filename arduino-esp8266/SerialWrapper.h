34d0c8f87cf9514bdc48c5692f8b925b602a6dc5
Radu Salagean <radusalagean@hotmail.com>
2020-11-23 21:40:53 +0200

    Add support for all print(ln) methods in the base wrapper

--- arduino-esp8266/SerialWrapper.h
+++ arduino-esp8266/SerialWrapper.h
@@ -9,12 +9,27 @@ private:
 
 public:
     virtual int available() = 0;
-    virtual void print(const char str[]) = 0;
-    virtual void print(char c) = 0;
-    virtual void println(const char str[]) = 0;
-    virtual void println(char c) = 0;
+    virtual byte peek() = 0;
     virtual byte read() = 0;
     virtual void write(byte b) = 0;
+
+    virtual void print(const char str[]) = 0;
+    virtual void print(char c) = 0;
+    virtual void print(unsigned char uc, int format = DEC) = 0;
+    virtual void print(int i, int format = DEC) = 0;
+    virtual void print(unsigned int ui, int format = DEC) = 0;
+    virtual void print(long l, int format = DEC) = 0;
+    virtual void print(unsigned long ul, int format = DEC) = 0;
+    virtual void print(double d, int decimals = 2) = 0;
+
+    virtual void println(const char str[]) = 0;
+    virtual void println(char c) = 0;
+    virtual void println(unsigned char uc, int format = DEC) = 0;
+    virtual void println(int i, int format = DEC) = 0;
+    virtual void println(unsigned int ui, int format = DEC) = 0;
+    virtual void println(long l, int format = DEC) = 0;
+    virtual void println(unsigned long ul, int format = DEC) = 0;
+    virtual void println(double d, int decimals = 2) = 0;
 };
 
 #endif
